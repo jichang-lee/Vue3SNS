96f0f8fdfaf645d7d004f720259febf18951d745
dlwlckdWkd12 <jccc23@naver.com>
2023-09-20 21:54:08 +0900

    layout

---
+++ src/components/ContainerBox.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <PostVue :data="data"/>
+        <PostVue/>
+        <PostVue/>
+    </div>
+</template>
+
+<script>
+import PostVue from './PostData.vue'
+
+
+export default {
+    name: 'containerVue',
+    components :{
+        PostVue
+    },
+    props : {
+        data : Array
+    }
+
+    
+}
+</script>
+
+
+<style >
+</style>
