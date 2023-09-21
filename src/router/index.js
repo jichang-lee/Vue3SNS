19266f033e07acd365a2807516280c26a14ae4b0
keunhyeong <keunhyeong@jlabsoft.com>
2023-09-21 18:17:57 +0900

    'routerRePush'

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+
+const routes = [
+    // {
+    //   path: '/',
+    //   redirect: 
+    // },
+ 
+  ];
+  
+  export default createRouter({
+    history: createWebHistory(),
+    routes,
+  });
